07e3624332270c00c517ce18ff69e38025d747f2
ram16230 <ram16230@uvg.edu.gt>
2017-01-20 21:58:30 -0600

    upload

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed = 3f;
+	public Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+
+	void Move(){
+		speed = 3f;
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate (new Vector2 (0, 1) * speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate (new Vector2 (0, -1) * speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (new Vector2 (-1, 0) * speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate (new Vector2 (1, 0) * speed * Time.deltaTime);
+		}
+
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		Debug.Log("hit");
+		speed = 0f;
+	}
+}
